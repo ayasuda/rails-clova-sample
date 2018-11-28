7ae98b32d4f0595f4b6d19a60350894deb2e7644
Atsushi Yasuda <atsushi.yasuda.jp@gmail.com>
2018-11-28 16:57:19 +0900

    root 及び dashboard コントローラを用意しました
    トップページと、ログイン直後のページを用意しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get '/dashboard', to: 'dashboard#index'
+  get 'root/index'
+  root to: 'root#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
